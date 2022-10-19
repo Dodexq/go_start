53fe4f40cdf411c4fcdb5b2c0162d4f024a71e06
Dodex <dodexpwnz@gmail.com>
2022-10-19 22:00:14 +0300

    chapter_3 start

--- chapter_2/exercises/ex1.go
+++ chapter_2/exercises/ex1.go
@@ -9,4 +9,8 @@ func main() {
 	var now time.Time = time.Now()
 	var year int = now.Year()
 	fmt.Println(year)
+	// or
+	today := time.Now().Day()
+	fmt.Println(today)
+	fmt.Println(time.Now().Date())
 }
---
+++ chapter_3/exercises/ex1.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("About one-third:", 1.0/3.0)
+	fmt.Printf("About one-third: %0.2f\n", 1.0/3.0)
+	resultString := fmt.Sprintf("About one-third: %0.2f\n", 1.0/3.0)
+	fmt.Printf(resultString)
+	fmt.Println("-----------------------------")
+	//
+	fmt.Printf("The %s cost %d cents each.\n", "gumballs", 23)
+	fmt.Printf("That will be $%f please.\n", 0.23*5)
+	fmt.Println("-----------------------------")
+	//
+	fmt.Printf("A float: %f\n", 3.1415)
+	fmt.Printf("An integer: %d\n", 15)
+	fmt.Printf("A string: %s\n", "hello")
+	fmt.Printf("A boolean: %t\n", false)
+	fmt.Printf("Values: %v %v %v\n", 1.2, "\t", true)
+	fmt.Printf("Values: %#v %#v %#v\n", 1.2, "\t", true)
+	fmt.Printf("Types: %T %T %T\n", 1.2, "\t", true)
+	fmt.Printf("Percent sign: %%\n")
+	fmt.Println("-----------------------------")
+	//
+	fmt.Printf("%12s | %s\n", "Product", "Cost in Cents")
+	fmt.Printf("-----------------------------\n")
+	fmt.Printf("%12s | %2d\n", "Stamps", 50)
+	fmt.Printf("%12s | %2d\n", "Paper Clips", 5)
+	fmt.Printf("%12s | %2d\n", "Tape", 99)
+	fmt.Println("-----------------------------")
+	//
+	fmt.Printf("%%7.3f: %7.3f\n", 12.3456)
+	fmt.Printf("%%7.2f: %7.2f\n", 12.3456)
+	fmt.Printf("%%7.1f: %7.1f\n", 12.3456)
+	fmt.Printf("%%.1f: %.1f\n", 12.3456)
+	fmt.Printf("%%.2f: %.2f\n", 12.3456)
+	fmt.Println("-----------------------------")
+
+}
