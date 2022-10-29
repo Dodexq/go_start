930fc07b2b77d4d3211d3596f89cf25b00aff611
Dodex <dodexpwnz@gmail.com>
2022-10-29 18:00:10 +0300

    update 29.10

--- chapter_4/exercises/tocelsium.go
+++ chapter_4/exercises/tocelsium.go
@@ -1,36 +1,19 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"keyboard"
 	"log"
-	"os"
-	"strconv"
-	"strings"
 )
-
-func getFloat() (float64, error) {
-	reader := bufio.NewReader(os.Stdin)
-	input, err := reader.ReadString('\n')
-	if err != nil {
-		return 0, err
-	}
-
-	input = strings.TrimSpace(input)
-	number, err := strconv.ParseFloat(input, 64)
-	if err != nil {
-		return 0, err
-	}
-	return number, nil
-}
 
 func celsium(fahrenheit float64) float64 {
 	return (fahrenheit - 32) * 5 / 9
 }
 
 func main() {
+
 	fmt.Print("Enter a temperature in Fahrenheit: ")
-	fahrenheit, err := getFloat()
+	fahrenheit, err := keyboard.GetFloat()
 	if err != nil {
 		log.Fatal(err)
 	}
