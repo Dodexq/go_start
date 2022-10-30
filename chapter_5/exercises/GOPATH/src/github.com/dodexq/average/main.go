fa2d954de0243acc16bea33ebccfd485fb6455a3
Dodex <dodexpwnz@gmail.com>
2022-10-30 19:17:07 +0300

    update 29.10

--- chapter_5/exercises/GOPATH/src/github.com/dodexq/average/main.go
+++ chapter_5/exercises/GOPATH/src/github.com/dodexq/average/main.go
@@ -1,10 +1,18 @@
 // avarage вычесляет среднее значение
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/dodexq/datafile"
+)
 
 func main() {
-	var numbers = [3]float64{71.2, 52.6, 84.2}
+	numbers, err := datafile.GetFloats("data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
 	var sum float64 = 0
 	for _, number := range numbers {
 		sum += number
